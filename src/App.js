0d804ec4d46964bf56201586d48eff486ed329e5
Bhumika Vasanta <bhumika.vasanta@quantiphi.com>
2023-10-27 01:09:40 +0530

    components folder added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import User from './components/User';
+import axios from 'axios';
+import SearchBar from './components/SearchBar';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchData, setSearchData] = useState([]);
+
+  const fetchData = async () => {
+    // const filePath = './UserInfo.json';
+    try {
+      const response = await axios.get('https://run.mocky.io/v3/e83076cb-5fe8-45cd-be9e-7de44a049cf4');
+      setData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    setSearchData(data.filter((x) => x.first.toLowerCase().includes(searchTerm.toLowerCase())
+    || x.first.toLowerCase().includes(searchTerm.toLowerCase())));
+    console.log(searchData);
+  }, [searchTerm]);
+
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="centered-container">
+      <div className="app-container">
+        List App
+      </div>
+      <div style={{ margin: '1rem' }}>
+        <SearchBar
+          searchTerm={searchTerm}
+          handleInputChange={handleInputChange}
+        />
+      </div>
+      {
+        searchTerm !== "" && searchData?.length === 0 && <div className="no-result">
+          No Results Found!!
+          </div>
+      }
+      {data.map((item, index) => (
+          <User
+            key={index}
+            item={item}
+            searchTerm={searchTerm}
+          />
+        ))}
     </div>
   );
 }
