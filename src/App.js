eaf30971a1ecdc758d2eac933a04cf1a0937c557
Bhumika Vasanta <bhumika.vasanta@quantiphi.com>
2023-10-28 00:12:08 +0530

    Loading text added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
   const [searchData, setSearchData] = useState([]);
 
   const fetchData = async () => {
-    // const filePath = './UserInfo.json';
     try {
       const response = await axios.get('https://run.mocky.io/v3/e83076cb-5fe8-45cd-be9e-7de44a049cf4');
       setData(response.data);
@@ -44,6 +43,11 @@ function App() {
           handleInputChange={handleInputChange}
         />
       </div>
+      {
+        data?.length === 0 && <div className="no-result">
+          Loading...
+          </div>
+      }
       {
         searchTerm !== "" && searchData?.length === 0 && <div className="no-result">
           No Results Found!!
